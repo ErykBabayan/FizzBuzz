a3b072d652ea2e24b493b529fae98304bcb59ba1
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2020-12-30 18:15:58 +0100

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_PD1
+{
+    class FizzBuzz
+    {
+        string fizzOrBuzz;
+        int number;
+
+
+        public string CheckFizzOrBuzz(string userInputNumber)
+        {
+            
+
+
+             if(!(int.TryParse(userInputNumber, out number)))
+            {
+                Console.WriteLine("Wprowadziłeś błędne dane");
+            }
+            else
+            {
+            if (number % 3 == 0 && number % 5 == 0)
+                fizzOrBuzz = "FizzBuzz";
+            else if (number % 3 == 0)
+                fizzOrBuzz = "Fizz";
+            else if (number % 5 == 0)
+                fizzOrBuzz = "Buzz";
+            else
+                fizzOrBuzz = number.ToString();
+            }
+
+                return fizzOrBuzz;
+        }
+
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_PD1
+{
+
+    /* Założenia:
+    if liczba % 3 == 0 Fizz
+    if liczba % 5 == 0 Buzz
+    if liczba % 3&&5 == 0 FizzBuzz
+    if liczba % 3&&5 != 0 liczba
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzOrBuzz = new FizzBuzz();
+
+
+            
+            
+            
+
+            Console.WriteLine("Wprowadź liczbe: ");
+            string userInputNumber = Console.ReadLine();
+            Console.WriteLine( fizzOrBuzz.CheckFizzOrBuzz(userInputNumber) ); 
+
+
+            
+
+        }
+
+
+
+
+    }
+}
